ae6dd96c76348539217cc5743f4973e57d319d76
tong <tww@feibo.com>
2019-08-16 14:29:10 +0800

    增加组件库

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Welcome to Your Vue.js App" :options="optyions"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from "@/components/labs/HelloWorld.vue";
 
 export default {
   name: "home",
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      optyions: {
+        msg: '标题改',
+        styleObject: {
+          background: '#fffaaa',
+        }
+      }
+    }
   }
 };
 </script>
